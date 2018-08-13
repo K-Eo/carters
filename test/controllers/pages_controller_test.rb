c2719ab614056646d0896d89cae0b198e00b3877
omar sanchez <omarto.300@gmail.com>
2018-08-13 18:24:27 -0500

    Corregit test

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get welcome" do
-    get pages_welcome_url
+  test "should get landing page" do
+    get root_url
     assert_response :success
   end
 end
