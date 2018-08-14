deccb91761c217834fce7b044aa1be350b5983a4
anjelhm <anjelhm@gmail.com>
2018-08-14 18:22:27 -0500

    agregar scaffold para sliders

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :items
+  resources :sliders
   get "dashboards/index"
   devise_for :users
   root to: "pages#welcome"
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_14_190044) do
+ActiveRecord::Schema.define(version: 2018_08_14_221540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "sliders", force: :cascade do |t|
+    t.boolean "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
