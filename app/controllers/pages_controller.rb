1796c053f9817846ac203896da33756c4e397137
Diego Lopez <aud0l@hotmail.com>
2018-08-17 15:00:29 -0500

    Agregar preload a imagenes de sliders

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   layout "landing"
 
   def welcome
-    @sliders = Slider.actives.all
+    @sliders = Slider.with_preloaded_image.actives.last(8)
     @items = Item.with_preloaded_images.last(4)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,9 @@
 class Item < ApplicationRecord
+  has_many_attached :images
+
   validates :title, presence: true
   validates :price, presence: true
   validates :description, presence: true
-  has_many_attached :images
 
   scope :with_preloaded_images, -> { preload(images_attachments: :blob) }
 end
