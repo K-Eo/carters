840304918d01bd26073d132abb943ea5f7a1db50
Diego Lopez <aud0l@hotmail.com>
2018-08-18 20:54:47 -0500

    Habilitar unaccent extension en pg

---
+++ db/migrate/20180819012453_enable_noaccents_extension.rb
@@ -0,0 +1,9 @@
+class EnableNoaccentsExtension < ActiveRecord::Migration[5.2]
+  def up
+    enable_extension "unaccent"
+  end
+
+  def down
+    disable_extension "unaccent"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_16_195122) do
+ActiveRecord::Schema.define(version: 2018_08_19_012453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "unaccent"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
