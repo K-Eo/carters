59c347fcc8eb93fd91bbabdb88d15f5683faea87
Diego Lopez <aud0l@hotmail.com>
2018-08-20 16:45:27 -0500

    Agregar scopes para ordenar Sliders

--- app/models/slider.rb
+++ app/models/slider.rb
@@ -3,4 +3,6 @@ class Slider < ApplicationRecord
 
   scope :actives, -> { where(active: true) }
   scope :with_preloaded_image, -> { preload(image_attachment: :blob) }
+  scope :order_by_active, -> { order(active: :desc) }
+  scope :order_by_newest, -> { order(created_at: :desc)}
 end
