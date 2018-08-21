7ef5ede1095d6eeb75789f2412319d30fa84c01a
Diego Lopez <aud0l@hotmail.com>
2018-08-21 16:44:43 -0500

    Actualizar seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+categories = []
+
+(1..10).each do |i|
+  category = Category.create(name: "category_#{i}")
+  categories.push(category.id)
+end
+
 200.times.each do |i|
   Item.create(
     title: Faker::Beer.name,
     price: Faker::Number.decimal(2),
-    description: Faker::Lorem.sentence
+    description: Faker::Lorem.sentence,
+    category_id: categories.sample
   )
 end
 
