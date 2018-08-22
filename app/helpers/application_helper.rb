bad6f8f6bd681acce8ed2752f3ceb10dee464ab5
Diego Lopez <aud0l@hotmail.com>
2018-08-22 00:44:56 -0500

    Extraer helper para active nav link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,18 @@ module ApplicationHelper
     link_to name, safe_params.merge(sort: sort), class: "dropdown-item"
   end
 
+  def active_nav_when(kontroller, action = nil)
+    klass = ["nav-link", "text-secondary"]
+
+    if kontroller == controller_name
+      if action.nil? || (action.present? && action == action_name)
+        klass.push("font-weight-bold")
+      end
+    end
+
+    klass.join(" ")
+  end
+
   private
     def safe_params
       params.except(:host, :post, :protocol).permit!
