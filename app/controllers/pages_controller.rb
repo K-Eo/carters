7c062c5b950f878a52e59bbd3308295277e04863
Diego Lopez <aud0l@hotmail.com>
2018-08-22 16:43:50 -0500

    Reorganizar layouts para evitar el scroll horizontal

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,4 @@
 class PagesController < ApplicationController
-  layout "landing"
-
   def welcome
     @sliders = Slider.with_attached_image.actives.last(8)
     @items = Item.with_preloaded_images.last(4)
