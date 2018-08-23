956a75cb880008ed262361601d5edd2eb794d718
Diego Lopez <aud0l@hotmail.com>
2018-08-22 23:50:57 -0500

    Agregar helper para obtener title e icon de nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,8 +16,35 @@ module ApplicationHelper
     klass.join(" ")
   end
 
+  def nav_title
+    if content_for?(:nav_title)
+      content_for(:nav_title)
+    else
+      content_tag :span, class: "navbar-brand text-secondary" do
+        title, icon = nav_title_lookup
+        concat octicon(icon, height: 24, width: 24, class: "d-inline-block align-top mr-3")
+        concat content_tag :span, title
+      end
+    end
+  end
+
   private
     def safe_params
       params.except(:host, :post, :protocol).permit!
+    end
+
+    def nav_title_lookup
+      default_lookup = "nav.#{controller_name}.#{action_name}"
+      fallback_lookup = "nav.#{controller_name}"
+
+      title = translate(:title, scope: default_lookup,
+        default: translate(:title, scope: fallback_lookup)
+      )
+
+      icon = translate(:icon, scope: default_lookup,
+        default: translate(:icon, scope: fallback_lookup)
+      )
+
+      [title, icon]
     end
 end
