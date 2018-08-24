2a57dac4597acb11d2c1ba6835e9ab514e8fa1f4
Diego Lopez <aud0l@hotmail.com>
2018-08-23 20:06:07 -0500

    Agregar clases para md

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,6 +1,6 @@
 module PagesHelper
   def create_service(title, description, icon)
-    content_tag :div, class: "col-12 p-4 mb-4" do
+    content_tag :div, class: "col-12 col-md-6 mx-auto p-4 mb-4" do
       content_tag :div, class: "shadow-sm text-center p-5" do
         concat octicon(icon, height: "60", class: "text-secondary")
         concat content_tag(:h6, title, class: "mt-3 font-weight-bold")
