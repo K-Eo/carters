1ec51fb27626223181230975cce308c8d2babaf6
Diego Lopez <aud0l@hotmail.com>
2018-08-25 00:10:14 -0500

    Agregar precision y scale a precio de item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_25_032853) do
+ActiveRecord::Schema.define(version: 2018_08_25_045916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 2018_08_25_032853) do
 
   create_table "items", force: :cascade do |t|
     t.string "title"
-    t.decimal "price"
+    t.decimal "price", precision: 15, scale: 2, default: "0.0", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
