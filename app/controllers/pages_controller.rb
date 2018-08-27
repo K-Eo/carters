5fd5999c5b882a9af926ee3729bc813bed532fdf
Diego Lopez <aud0l@hotmail.com>
2018-08-27 14:56:28 -0500

    Agregar productos a página de inicio

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
   def welcome
     @sliders = Slider.with_attached_image.actives.last(8)
-    @items = Item.includes(:category).with_attached_cover.last(4)
+    @items = Item.includes(:category)
+                 .with_attached_cover
+                 .order(with_discount: :desc)
+                 .order(updated_at: :desc)
+                 .order(created_at: :desc)
+                 .take(24)
   end
 
   def contact
